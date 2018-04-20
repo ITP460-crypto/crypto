3bdd1100898476db96f4a042c22794b3931f8dba
Ryan Selden <seldenr@catlin.edu>
2018-04-19 20:05:01 -0700

    implemented login and logout fully

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -10,63 +10,75 @@ var config = {
 firebase.initializeApp(config);
 
 function createAccount() {
-  //alert("works")
-  var email = $("#email").val()
-  var psk = $("#password").val()
-  firebase.auth().createUserWithEmailAndPassword(email, psk).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("Hmm, something went wrong...")
-    console.log(`${errorCode}, ${errorMessage}`)
-    // ...
-  });
-  alert("Account created! You can now login.")
-  //window.location.href = "/"
-}
-
-function login() {
   var email = $("#email").val()
   var password = $("#password").val()
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    console.log(`Firebase error during createAccount: ${error.code}, ${error.message}`)
+    alert("This isn't working right now, try again later.")
+  }).then(function() {
+    doLogin(email, password, function() {
+      window.location.href = "/dashboard/"
+    })
+  })
+}
+
+function doLogin(email, password, then) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     //alert("Hmm, something went wrong...")
+    console.log(`Firebase error during doLogin: ${errorCode}, ${errorMessage}`)
     sessionStorage.setItem("loggedIn", false)
     // ...
   }).then(function(user) {
     if (typeof(user) === "undefined") {
       alert("Hmm, that username/password isn't correct.")
       sessionStorage.setItem("loggedIn", false)
+      console.log("Was unable to log in!")
     } else {
       sessionStorage.setItem("loggedIn", true)
       sessionStorage.setItem("email", email)
       sessionStorage.setItem("password", password)
-      window.location.href = "/dashboard/"
+      console.log(`Logged in successfully as ${email}`)
+      //if they go to the homepage but they're already signed in, send them to the dash
+      if (window.location.href.indexOf("dashboard") < 0) {
+        window.location.href = "/dashboard/"
+      }
+      if (typeof($("#accountname")[0]) !== "undefined") {
+        console.log("attempting to write accountname")
+        $("#accountname").html(email)
+      } else {
+        console.log("not attempting to write accountname")
+      }
+      then()
     }
   });
 }
 
+function login() {
+  var email = $("#email").val()
+  var password = $("#password").val()
+  doLogin(email, password, function() {
+    window.location.href = "/dashboard/"
+  })
+}
+
+function logout() {
+  firebase.auth().signOut().then(function() {
+    sessionStorage.setItem("loggedIn", false)
+    sessionStorage.setItem("email", "")
+    sessionStorage.setItem("password", "")
+    window.location.href = "/"
+  }).catch(function(error) {
+    alert("Couldn't log out. Please close the window.")
+  });
+}
+
 $(document).ready(function() {
-  console.log("running")
   if (sessionStorage.getItem("loggedIn") === "true") {
     var email = sessionStorage.getItem("email")
     var password = sessionStorage.getItem("password")
-    console.log(email)
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert("Hmm, something went wrong...")
-      // ...
-    }).then(function(user) {
-      if (typeof(user) === "undefined") {
-        alert("Hmm, that username/password isn't correct.")
-      } else {
-        sessionStorage.setItem("loggedIn", true)
-        console.log("loggedIn")
-      }
-    });
+    doLogin(email, password, function(){})
   }
 })
